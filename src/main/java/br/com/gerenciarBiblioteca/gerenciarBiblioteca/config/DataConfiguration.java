9f05d49811fa192b71165d6825b0f29d8fc43f30
unknown <juliopvinf@gmail.com>
2018-06-09 10:09:02 -0400

    Adicionando classe DataConfiguration

---
+++ src/main/java/br/com/gerenciarBiblioteca/gerenciarBiblioteca/config/DataConfiguration.java
@@ -0,0 +1,42 @@
+package br.com.gerenciarBiblioteca.gerenciarBiblioteca.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+@PropertySource( value={
+		"classpath:application.properties"
+})
+public class DataConfiguration {
+	
+	@Bean
+	public DataSource dataSource(){
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/gerenciar_biblioteca");
+		dataSource.setUsername("root");
+		
+		return dataSource;
+	}
+	
+	public JpaVendorAdapter jpaVendorAdapter(){
+		
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		
+		return adapter;
+	}
+
+}
